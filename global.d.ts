6f5598e2ec88e0fd6a8d6e9f57b15abbd504ec68
BongominErickJuma <ericbongomin@gmail.com>
2024-12-21 20:04:47 +0300

    deleted unecessary files

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "jspdf" {
-  interface jsPDF {
-    autoTable: (options: {
-      head: string[][];
-      body: (string | number)[][];
-    }) => void;
-  }
-}
--- src/types/types.ts
+++
@@ -1,9 +0,0 @@
-// src/types.ts
-export interface Report {
-  transactionId: string;
-  date: string;
-  accountHolderName: string;
-  transactionType: string;
-  amount: number;
-  accountBalance: number;
-}
